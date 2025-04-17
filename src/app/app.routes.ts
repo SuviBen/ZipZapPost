c9ee211da2ab91080d70e3cbfc070c060ae0e94c
iSPMaster <beni.badulescu@gmail.com>
2025-04-17 10:20:16 +0200

    Added router, ready to use

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
+import { LoginComponent } from './containers/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
